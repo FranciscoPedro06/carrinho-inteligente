a7887f1bfc098f69f3f8afb67f111033eefe8b05
FranciscoPedro06 <pedro2006francisco@gmail.com>
2025-09-18 20:52:40 -0300

    Adicionando o SpringBoot

---
+++ src/main/java/br/com/carrinhoInteligente/MainSpringBoot.java
@@ -0,0 +1,13 @@
+package br.com.carrinhoInteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainSpringBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainSpringBoot.class, args);
+    }
+
+}
